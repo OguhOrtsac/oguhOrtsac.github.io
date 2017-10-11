986c8c1e0951afa0efc75e9883bd92927986ec80
OguhOrtsac <oguhortsac@hotmail.com>
2017-10-11 11:58:03 -0500

    Add files via upload

---
+++ JavaScript/DOM/main.js
@@ -0,0 +1,18 @@
+var cajas = document.getElementsByName('div');
+
+function agregaCaja(texto)
+{
+    var nuevo= document.createElement('div');
+}
+
+// var entrada= document.getElementById('entrada');  // es para acceder a un objeto 
+// var contenido = entrada.value;           obtiene el valor del objeto
+// agregaCaja(contenido);
+
+ /*event handler   
+      Es unico  solo puede llamar a una funcion 
+*/
+
+ /* Event listener 
+      Se puede llamar muchas veces a una funcion
+ */
